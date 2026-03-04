57da04df8ca2359b3ddf3b9981cac94c1c097965
bigleggydan <115162689+bigleggydan@users.noreply.github.com>
2026-03-04 15:46:22 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA-Iy2Uli9_9ebdJZyVK5RpLKwZF3XrdY4",
+  authDomain: "lirtr-2fc4d.firebaseapp.com",
+  projectId: "lirtr-2fc4d",
+  storageBucket: "lirtr-2fc4d.firebasestorage.app",
+  messagingSenderId: "7328042961",
+  appId: "1:7328042961:web:3b7851c755290408c55af0",
+  measurementId: "G-EGH879VZPJ"
+};
